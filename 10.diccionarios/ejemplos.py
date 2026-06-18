e70de39c9616e814f5fe3b7c120041a06ba0366d
parionaluz4-commits <parionaluz4@gmail.com>
2026-06-18 08:57:29 -0500

    tarea libreria estandar

---
+++ 10.diccionarios/ejemplos.py
@@ -0,0 +1,8 @@
+# modulos y libreria estandar 
+# libreria estandar typing tipar datos a list y diccionarios para hacer mas optimo el codigo
+# modulo es una porcion de codigo utilizable para poder usarlo nesecitamos importar la parte del codigo que deseamos utilizar.
+
+# en este codigo estoy importando desde la libreria typing la funcion union
+from typing import union
+
+alumno:dict[str:str|int]
